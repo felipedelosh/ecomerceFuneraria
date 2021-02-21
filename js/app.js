45a1a333324e8760f27ce53e3f0433a8b4820c7e
felipedelosh <doctorfhernandez@hotmail.com>
2021-02-21 14:22:24 -0500

    insert jquery to go external social pages

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//Wait to charge all html+css
+$(document).ready(function(){
+
+    //Goto to main page
+    $('#goToMainPage').click(function(){
+        location.reload();
+    //End go to facebbok    
+    });
+
+    //Go to facebook
+    $('#goToFacebook').click(function(){
+        url = "http://www.facebook.com";
+        window.open(url, "_blank")
+    //End go to facebbok    
+    });
+
+    //Go to Instagram
+    $('#goToInstagram').click(function(){
+        url = "http://www.instagram.com";
+        window.open(url, "_blank")
+    //End go to Instagram   
+    });
+
+});
