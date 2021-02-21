fe223a495a4b1a316c6da9fa5cfd49d3e64317fb
felipedelosh <doctorfhernandez@hotmail.com>
2021-02-21 14:51:12 -0500

    put currently year

--- js/app.js
+++ js/app.js
@@ -10,15 +10,20 @@ $(document).ready(function(){
     //Go to facebook
     $('#goToFacebook').click(function(){
         url = "http://www.facebook.com";
-        window.open(url, "_blank")
+        window.open(url, "_blank");
     //End go to facebbok    
     });
 
     //Go to Instagram
     $('#goToInstagram').click(function(){
         url = "http://www.instagram.com";
-        window.open(url, "_blank")
+        window.open(url, "_blank");
     //End go to Instagram   
     });
+
+
+    //Put a currently year on the page
+    var currentlyDate = new Date();
+    $("#copyright").text(currentlyDate.getFullYear());
 
 });
